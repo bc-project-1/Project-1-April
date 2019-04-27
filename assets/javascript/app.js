c925901349ab363bad6d8f38184653009363d366
Zeke <ezekielbadowski@gmail.com>
2019-04-27 14:10:44 -0500

    Added Local Storage for adding Favs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,3 +4,22 @@ function writeUber(response){
     $('.uber-cost').val(response.prices[4].high_estimate + ' dollars');
 }
 
+function addFav(){
+   var fav = {
+       label: $('#fav-label').val(),
+       lat: sessionStorage.getItem("eLat"),
+       lon: sessionStorage.getItem("eLong")
+   }
+   
+   var a = [];
+    console.log('**********',localStorage.getItem('favorites') );
+       
+    if(localStorage.getItem('favorites') != null){
+        a = JSON.parse(localStorage.getItem('favorites'));
+    }
+    
+    a.push(fav);
+    
+    localStorage.setItem('favorites', JSON.stringify(a));
+}
+
