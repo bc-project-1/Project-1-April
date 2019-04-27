266bc3ca8d70b50f62289a8977d8c2337835d7ef
Nickolas Gamblin <nickgamblin@gmail.com>
2019-04-27 13:11:31 -0500

    set weather api return to variables and appended to dom

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,25 @@
 
 
-// $("#submit-destination").on("click", function (event) {
-//     event.preventDefault();
-//     //debugger;
-//     window.location = 'Results.html';
-//     console.log(sessionStorage.getItem("sLat"))
-//     console.log(sessionStorage.getItem("sLong"))
-//     console.log(sessionStorage.getItem("eLat"))
-//     console.log(sessionStorage.getItem("eLong"))
+$("#submit-destination").on("click", function (event) {
+    event.preventDefault();
+    //debugger;
+    window.location = 'Results.html';
+    console.log(sessionStorage.getItem("sLat"))
+    console.log(sessionStorage.getItem("sLong"))
+    console.log(sessionStorage.getItem("eLat"))
+    console.log(sessionStorage.getItem("eLong"))
 
-//     sessionStorage.getItem("sLat"), sessionStorage.getItem("sLong"), sessionStorage.getItem("eLat"), sessionStorage.getItem("eLong"),
+    sessionStorage.getItem("sLat"), sessionStorage.getItem("sLong"), sessionStorage.getItem("eLat"), sessionStorage.getItem("eLong"),
 
-//     getUberApi(sessionStorage.getItem("sLat"), sessionStorage.getItem("sLong"), sessionStorage.getItem("eLat"), sessionStorage.getItem("eLong"));
+    getUberApi(sessionStorage.getItem("sLat"), sessionStorage.getItem("sLong"), sessionStorage.getItem("eLat"), sessionStorage.getItem("eLong"));
 
-// });
+});
+
+
+// function weatherToDom(city, temp, currWeather){
+//     console.log("Dest. Temperature: " + temp)
+//     console.log("Dest. Weather: " + currWeather)
+//     $("#city").append(city)
+//     $("#temp").append(temp)
+//     $("#condtions").append(currWeather)
+// }
