23f391da3fa6a98484a45e6ff2fd1a0b43ba732d
Zeke <ezekielbadowski@gmail.com>
2019-04-27 11:48:01 -0500

    Uber api call after submit click
    script on results page calls uber api using session storage
    IP look up call also moved to index page to not waste resources on results

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+
+
+// $("#submit-destination").on("click", function (event) {
+//     event.preventDefault();
+//     //debugger;
+//     window.location = 'Results.html';
+//     console.log(sessionStorage.getItem("sLat"))
+//     console.log(sessionStorage.getItem("sLong"))
+//     console.log(sessionStorage.getItem("eLat"))
+//     console.log(sessionStorage.getItem("eLong"))
+
+//     sessionStorage.getItem("sLat"), sessionStorage.getItem("sLong"), sessionStorage.getItem("eLat"), sessionStorage.getItem("eLong"),
+
+//     getUberApi(sessionStorage.getItem("sLat"), sessionStorage.getItem("sLong"), sessionStorage.getItem("eLat"), sessionStorage.getItem("eLong"));
+
+// });
