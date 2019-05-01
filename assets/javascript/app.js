f3acfd77fbfc712b81f6b48cef80081480044fd1
Anthony Gaimari <tgaimari@comcast.net>
2019-05-01 10:14:20 -0500

    Updates from tuesday night
    QA and refactoring of items on webpages

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 //Uber Dom manipulation function
 function writeUber(response){
-    $('.uber-time').val(response.prices[4].duration / 60 + ' minutes');
-    $('.uber-cost').val(response.prices[4].high_estimate + ' dollars');
+    $('.uber-time').text(response.prices[4].duration / 60 + ' minutes');
+    $('.uber-cost').text(response.prices[4].high_estimate + ' dollars');
 }
 
 
