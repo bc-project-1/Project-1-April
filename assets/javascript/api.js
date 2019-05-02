1cacff642e5c4171a512eedf20bc2b4bb7daa71a
Nickolas Gamblin <nickgamblin@gmail.com>
2019-05-01 19:49:55 -0500

    google direction api calls are working and appending to DOM... ugly

--- assets/javascript/api.js
+++ assets/javascript/api.js
@@ -1,7 +1,3 @@
-
-
-
-
 //User location through IP api
 function ipLookUp() {
     $.ajax('http://ip-api.com/json')
@@ -24,9 +20,7 @@ function ipLookUp() {
         );
 }
 
-
 // ================== Google Map Variables and Calls ================================================
-
 
 function startMap() {
     var myLatlng = new google.maps.LatLng(sessionStorage.getItem("sLat"), sessionStorage.getItem("sLong"));
@@ -45,18 +39,7 @@ function startMap() {
 
 }
 
-
-
-
-
-
-
-
-
-// Uber api call
-
 // ================== Uber api call ================================================================
-
 
 function getUberApi(startLat, startLong, endLat, endLong) {
     var queryURL = 'https://cors-anywhere.herokuapp.com/api.uber.com/v1.2/estimates/price?';
@@ -78,19 +61,17 @@ function getUberApi(startLat, startLong, endLat, endLong) {
         });
 }
 
-
 // ================== Weather api call ================================================================
 
 function getWeather() {
-
-var lat= sessionStorage.getItem("eLat");
-var lon= sessionStorage.getItem("eLong");
-queryURL = 'http://api.openweathermap.org/data/2.5/weather?';
-weatherLat = 'lat=' + lat;
-weatherLon = '&lon=' + lon;
-accessKey = '&APPID=0ff132ddd83d15f772c6169f2ee83a2b';
-units = "&units=imperial"
-queryURL += weatherLat + weatherLon + units + accessKey;
+    var lat= sessionStorage.getItem("eLat");
+    var lon= sessionStorage.getItem("eLong");
+    queryURL = 'http://api.openweathermap.org/data/2.5/weather?';
+    weatherLat = 'lat=' + lat;
+    weatherLon = '&lon=' + lon;
+    apiKey = '&APPID=0ff132ddd83d15f772c6169f2ee83a2b';
+    units = "&units=imperial"
+    queryURL += weatherLat + weatherLon + units + apiKey;
 
 $.ajax({ url: queryURL, method: "GET" })
     .then(function (response) {
@@ -105,4 +86,51 @@ $.ajax({ url: queryURL, method: "GET" })
         $("#currWeather").append(" "+currWeather)
         // weatherToDom()
     });
+}
+
+function getTransit() {
+    var destinationLat= sessionStorage.getItem("eLat");
+    var destinationLon= sessionStorage.getItem("eLong");
+    var startingLat= sessionStorage.getItem("sLat");
+    var startingLon= sessionStorage.getItem("sLong");
+
+    // test url below - just supply the key at the end
+    // queryURL = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=41.896280,-87.618851&destinations=41.950083%2C-87.647746&key=';
+    // queryURL = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins='
+    apiKey= ''
+    console.log("before call // transit")
+    queryURL += startingLat + "," + startingLon + "&destinations=" + destinationLat + "," + destinationLon + apiKey
+     $.ajax({ url: queryURL, method: "GET"})
+        .then(function (response) {
+            console.log("running"); 
+            console.log(response);
+            var transitDuration = response.rows[0].elements[0].duration.text
+            var transitDistance = response.rows[0].elements[0].distance.text
+            $("#transitDuration").append(transitDuration)
+            $("#transitDistance").append(" "+transitDistance)        
+
+        });
+}
+
+function getWalking() {
+    var destinationLat= sessionStorage.getItem("eLat");
+    var destinationLon= sessionStorage.getItem("eLong");
+    var startingLat= sessionStorage.getItem("sLat");
+    var startingLon= sessionStorage.getItem("sLong");
+    apiKey= ''
+    // test url below- just supply key
+    queryURL = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=41.896280,-87.618851&destinations=41.950083%2C-87.647746&mode=walking&key=';
+     console.log("before call")
+     console.log("walking")
+
+    $.ajax({ url: queryURL, method: "GET"})
+        .then(function (response) {
+            console.log("running"); 
+            console.log(response);
+            var walkingDuration = response.rows[0].elements[0].duration.text
+            var walkingDistance = response.rows[0].elements[0].distance.text
+            $("#walkingDuration").append(" "+walkingDuration)
+            $("#walkingDistance").append(" "+walkingDistance)   
+
+        });
 }
